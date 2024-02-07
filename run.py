85c96a4394e74778dc00f9ed6f29532825840fa7
hulalu12 <159125709+hulalu12@users.noreply.github.com>
2024-02-07 04:28:06 -0800

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,10 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 
+# Import necessary libraries
+import cv2
+import numpy as np
+
 # Load the YOLOv8 model
 model = YOLO('./model/video-seg.pt')
 
@@ -10,25 +14,27 @@ video_path = "./data/exercise.mp4"
 cap = cv2.VideoCapture(video_path)
 
 # Loop through the video frames
-a11=True
 while cap.isOpened():
     # Read a frame from the video
     success, frame = cap.read()
 
     if success:
-        # Run YOLOv8 tracking on the frame, persisting tracks between frames
-        results = model.predict(frame,imgsz=(720,1280),iou=1)
+        # Run predict on the frame, persisting tracks between frames
+        results = model.predict(frame, imgsz=(720,1280))
         masks = results[0].masks
         mask1 = masks[0]
-        tensor_cpu =  mask1.data[0].cpu()
+        tensor_cpu = mask1.data[0].cpu()
         mask = tensor_cpu.numpy()
-        new_mask=mask.astype(np.uint8)
-        image_frame = cv2.bitwise_and(frame, frame, mask=mask4)
+        new_mask = mask.astype(np.uint8)
         
-        cv2.imshow("video Tracking", image_frame)
+        # Apply the mask to the frame
+        image_frame = cv2.bitwise_and(frame, frame, mask=new_mask)
+        
+        # Display the frame with tracking
+        cv2.imshow("Video Tracking", image_frame)
 
         # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord("q"):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         # Break the loop if the end of the video is reached
